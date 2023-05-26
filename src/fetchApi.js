ed4187caa1621a1dd4590eb2576c3ab92b1b779d
Aleksandr Smirnov <aa.smirnov2@gmail.com>
2023-05-25 20:03:15 -0600

    replace domain name with ip address

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://be.thoughtsporadic.com';
+const baseUrl = 'http://54.160.87.55:8080';
 
 export const fetchAllPosts = chosenTag => {
     const postsUrl = baseUrl + '/posts' + (chosenTag ? `/tag/${chosenTag}` : '');
